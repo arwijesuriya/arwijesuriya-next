051abba39b340b51b222ff20455fb5220bb152f8
Achira Wijesuriya <wijesuriya4me@gmail.com>
2024-05-19 18:49:56 +0530

    install framer motion

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,12 +3,13 @@ import "./globals.css";
 
 // ADD COMPONENTS
 import Header from "@/components/Header";
+import PageTransition from "@/components/PageTransition";
 
 const jetbrainsMono = JetBrains_Mono({ 
-    subsets: ["latin"],
-    weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
-    variable: '--font-jetbrainsMono' 
-  });
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable: '--font-jetbrainsMono' 
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -20,7 +21,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={jetbrainsMono.variable}>
         <Header/>
-        {children}
+        <PageTransition>
+          {children}
+        </PageTransition>
       </body>
     </html>
   );
---
+++ components/PageTransition.jsx
@@ -0,0 +1,20 @@
+"use client";
+
+// ADD FRAMER MOTION
+import { AnimatePresence, motion } from "framer-motion"
+import { usePathname } from "next/navigation";
+
+const PageTransition = ({ children }) => {
+
+    const pathname = usePathname();
+
+    return (
+        <AnimatePresence>
+            <div key={pathname}>
+                {children}
+            </div>
+        </AnimatePresence>
+    )
+}
+
+export default PageTransition
