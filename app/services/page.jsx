7b0c63401d1b74a88de49000945b4dad1c61973a
Achira Wijesuriya <wijesuriya4me@gmail.com>
2024-05-20 23:32:43 +0530

    Update page.jsx

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -34,7 +34,7 @@ import { motion } from "framer-motion";
 
 const Services = () => {
   return (
-    <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+    <section className="min-h-[70vh] flex flex-col justify-center py-10 xl:py-0">
       <div className="container mx-auto">
         <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn"}}} className="grid grid-cols-1 md:grid-cols-2 gap-[60px]">
           {services.map((service, index) => {
@@ -42,13 +42,13 @@ const Services = () => {
               <div key={index} className="flex-1 flex flex-col justify-center gap-6 group">
                 <div className="w-full flex justify-between items-center">
                   <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">{service.num}</div>
-                  <Link href={service.href} className="w-[60px] h-[60px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45" >
+                  <Link href={service.href} className="w-[50px] h-[50px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45" >
                     <BsArrowDownRight className="text-primary text-3xl" />
                   </Link>
                 </div>
 
                 {/* TITLE */}
-                <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500" >
+                <h2 className="text-[40px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500" >
                   {service.title}
                 </h2>
 
